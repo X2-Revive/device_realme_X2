d9349ed54e328f428d64bb11c6a5ac942ea89892
Alessio Balsini <balsini@google.com>
2022-01-18 16:25:51 +0300

    X2: Use FUSE passthrough by default Enable the persist.sys.fuse.passthrough.enable flag in the system configuration to enable the FUSE passthrough feature. This feature has been enabled on Cuttlefish devices for about two months and no issues have been detected yet.
    * Also added ro.charger.enable_suspend
    
    Bug: 168023149
    Test: 'adb shell getprop | grep persist.sys.fuse.passthrough.enable',
        ScopedStorageTest
    Signed-off-by: Alessio Balsini <balsini@google.com>
    Change-Id: I874e2e2bd143513c32aedf27b824baccc5fcc478

--- device.mk
+++ device.mk
@@ -407,6 +407,10 @@ PRODUCT_SYSTEM_SERVER_COMPILER_FILTER := speed-profile
 PRODUCT_USE_PROFILE_FOR_BOOT_IMAGE := true
 PRODUCT_DEX_PREOPT_BOOT_IMAGE_PROFILE_LOCATION := frameworks/base/config/boot-image-profile.txt
 
+# Storage
+PRODUCT_PROPERTY_OVERRIDES += \
+    persist.sys.fuse.passthrough.enable=true
+
 # Telephony
 PRODUCT_PACKAGES += \
     qti-telephony-hidl-wrapper \

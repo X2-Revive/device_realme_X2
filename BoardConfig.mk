50e43a2f6d3dcffda730e54d65a08dbd49fd1997
Dyneteve <sound0020@gmail.com>
2022-06-15 14:47:41 +0000

    X2: configs: Switch To AOSP surfaceflinger
    * AOSP SurfaceFlinger is smoother than CAF
    
    Signed-off-by: Dyneteve <sound0020@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -170,6 +170,9 @@ SELINUX_IGNORE_NEVERALLOWS := true
 TARGET_SURFACEFLINGER_UDFPS_LIB := //$(DEVICE_PATH):libudfps_extension.x2
 TARGET_USES_FOD_ZPOS := true
 
+# SurfaceFlinger
+TARGET_USE_AOSP_SURFACEFLINGER := true
+
 # Verified Boot
 BOARD_AVB_ENABLE := true
 BOARD_AVB_MAKE_VBMETA_IMAGE_ARGS += --set_hashtree_disabled_flag

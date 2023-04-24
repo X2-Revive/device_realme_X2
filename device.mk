f324e3dee7999a05a3fdc6bdd91b00dd6bd54a36
kubersharma001 <kubersharma001@gmail.com>
2023-04-24 12:14:34 +0600

    X2: Switch to AOSP WFD
    Signed-off-by: sarthakroy2002 <sarthakroy2002@gmail.com>
    Change-Id: I1ae32e41ea3cdb1f3b5d7f81a57f548fa211c9ca

--- device.mk
+++ device.mk
@@ -401,14 +401,5 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/wifi/p2p_supplicant_overlay.conf:$(TARGET_COPY_OUT_VENDOR)/etc/wifi/p2p_supplicant_overlay.conf \
     $(LOCAL_PATH)/wifi/wpa_supplicant_overlay.conf:$(TARGET_COPY_OUT_VENDOR)/etc/wifi/wpa_supplicant_overlay.conf
 
-# WiFi Display
-PRODUCT_PACKAGES += \
-    android.media.audio.common.types-V2-cpp \
-    libnl \
-    libwfdaac_vendor
-
-PRODUCT_BOOT_JARS += \
-    WfdCommon
-
 # Inherit from vendor if exists
 $(call inherit-product-if-exists, vendor/realme/X2/X2-vendor.mk)

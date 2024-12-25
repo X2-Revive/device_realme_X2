5ea8a75d455ce8dff6b5605b776b76d55b4fa38f
LuK1337 <priv.luk@gmail.com>
2024-12-25 23:42:18 +0100

    X2: Switch to NCM USB tethering
    Change-Id: I20b6b2b2e26a45d8b28c7483afc83ad4da4b8643

--- device.mk
+++ device.mk
@@ -16,6 +16,9 @@ DEVICE_PACKAGE_OVERLAYS += \
     $(LOCAL_PATH)/overlay \
     $(LOCAL_PATH)/overlay-lineage
 
+PRODUCT_PACKAGES += \
+    NcmTetheringOverlay
+
 PRODUCT_CHARACTERISTICS := nosdcard
 
 # Additional native libraries

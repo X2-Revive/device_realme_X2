24354f52f5b503bbb59092877b491a759bcead37
Michael Bestas <mkbestas@lineageos.org>
2025-01-01 03:50:53 +0200

    X2: Update display flags & packages
    Required after display HAL blueprint conversion
    
    Change-Id: Ifc4e10703d3424607b9dc8bcc8e417d1c88cc2f3

--- device.mk
+++ device.mk
@@ -132,8 +132,8 @@ PRODUCT_PACKAGES += \
     android.hardware.graphics.mapper@4.0-impl-qti-display \
     vendor.qti.hardware.display.allocator-service \
     vendor.qti.hardware.memtrack-service \
-    gralloc.sm6150 \
-    hwcomposer.sm6150 \
+    gralloc.qcom \
+    hwcomposer.qcom \
     libqdMetaData \
     libqdMetaData.system \
     libtinyxml \

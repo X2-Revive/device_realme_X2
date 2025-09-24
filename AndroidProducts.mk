c6ec3f7776277819be29b213c36176d95b5b7478
RJTangpos <rjtangpos39@gmail.com>
2025-09-24 12:12:37 +0800

    X2: Bringup Infinity-X

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -5,8 +5,8 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/lineage_X2.mk
+    $(LOCAL_DIR)/infinity_X2.mk
 
 COMMON_LUNCH_CHOICES := \
-    lineage_X2-userdebug \
-    lineage_X2-eng
+    infinity_X2-userdebug \
+    infinity_X2-eng
--- infinity_X2.mk
+++ infinity_X2.mk
@@ -10,14 +10,23 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_p.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/non_ab_device.mk)
 
-# Inherit some common Lineage stuff.
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+# Inherit some common Infinity stuff.
+$(call inherit-product, vendor/infinity/config/common_full_phone.mk)
 
 # Inherit from X2 device
 $(call inherit-product, device/realme/X2/device.mk)
 
+# Infinity stuff
+TARGET_BOOT_ANIMATION_RES := 1080
+SYSTEM_OPTIMIZE_JAVA := true
+SYSTEMUI_OPTIMIZE_JAVA := true
+TARGET_HAS_UDFPS := true
+TARGET_DISABLE_EPPE := true
+WITH_GAPPS := false
+INFINITY_MAINTAINER := ꧁𓊈𒆜༺𝓗𝖊𝖝𝖊𝖓𝖟𝖎𝖗𝖐𝖊𝖑,{𝕽}༻𒆜𓊉꧂
+
 # Device identifier. This must come after all inclusions.
-PRODUCT_NAME := lineage_X2
+PRODUCT_NAME := infinity_X2
 PRODUCT_DEVICE := X2
 PRODUCT_BRAND := realme
 PRODUCT_MODEL := realme X2

2f7318b6bd87a645ab0243e0dfa0f9ed13890b3f
RJTangpos <rjtangpos39@gmail.com>
2025-10-14 10:01:59 +0800

    X2: Set vendor and udfps init lib via soong config

--- device.mk
+++ device.mk
@@ -418,9 +418,15 @@ PRODUCT_PACKAGES += \
 PRODUCT_BOOT_JARS += \
     telephony-ext
 
+# UDFPS Init
+$(call soong_config_set,surfaceflinger,udfps_lib,//$(LOCAL_PATH):libudfps_extension.x2)
+
 # USB
 PRODUCT_PACKAGES += \
     android.hardware.usb@1.0-service
+
+# Vendor Init
+$(call soong_config_set,libinit,vendor_init_lib,//$(LOCAL_PATH):libinit_x2)
 
 # Vendor service manager
 PRODUCT_PACKAGES += \
